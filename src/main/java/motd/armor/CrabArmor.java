d1d92330c87a27562b480c158fecc8fb6dfee597
Daniel Hajjar <daniel.hajjar@hotmail.se>
2018-05-31 19:55:39 +0200

    added crab armor, crab shell piece, and crab armor recipes

---
+++ src/main/java/motd/armor/CrabArmor.java
@@ -0,0 +1,38 @@
+package motd.armor;
+
+import motd.References;
+import motd.handlers.MOTDItems;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class CrabArmor extends ItemArmor {
+	
+	private static ArmorMaterial material = EnumHelper.addArmorMaterial("crab", References.MODID + ":crab", 15, new int[] {2, 5, 5, 2}, 14, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 2.0F);
+	
+	public CrabArmor(String name, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(material, renderIndexIn, equipmentSlotIn);
+		this.setUnlocalizedName(name);
+		this.setRegistryName(References.MODID, name);
+		this.setCreativeTab(CreativeTabs.COMBAT);
+	}
+
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
+		if (player.inventory.armorItemInSlot(0).getItem() == MOTDItems.crab_boots &&
+			player.inventory.armorItemInSlot(1).getItem() == MOTDItems.crab_leggings &&
+			player.inventory.armorItemInSlot(2).getItem() == MOTDItems.crab_chestplate &&
+			player.inventory.armorItemInSlot(3).getItem() == MOTDItems.crab_helmet
+		) {
+			player.addPotionEffect(new PotionEffect(Potion.getPotionById(13), 0, 1));
+		}
+	}
+	
+}
--- src/main/java/motd/handlers/MOTDItems.java
+++ src/main/java/motd/handlers/MOTDItems.java
@@ -1,10 +1,13 @@
 package motd.handlers;
 
+import motd.armor.CrabArmor;
 import motd.items.BaseItem;
 import motd.items.ItemEmeraldIdol;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
@@ -20,19 +23,35 @@ public class MOTDItems {
 	public static Item iron_ring;
 	public static Item prismarin_cross;
 	
+	public static Item crab_shell_piece;
+	
+	public static ItemArmor crab_helmet;
+	public static ItemArmor crab_chestplate;
+	public static ItemArmor crab_leggings;
+	public static ItemArmor crab_boots;
+	
 	public static void init() {
 		emerald_idol = new ItemEmeraldIdol();
 		diamond_skull = new BaseItem("diamond_skull", CreativeTabs.MISC);
 		gold_tablet = new BaseItem("gold_tablet", CreativeTabs.MISC);
 		iron_ring = new BaseItem("iron_ring", CreativeTabs.MISC);
 		prismarin_cross = new BaseItem("prismarin_cross", CreativeTabs.MISC);
+		
+		crab_shell_piece = new BaseItem("crab_shell_piece", CreativeTabs.MATERIALS);
+		
+		crab_helmet = new CrabArmor("crab_helmet", 1, EntityEquipmentSlot.HEAD);
+		crab_chestplate = new CrabArmor("crab_chestplate", 1, EntityEquipmentSlot.CHEST);
+		crab_leggings = new CrabArmor("crab_leggings", 2, EntityEquipmentSlot.LEGS);
+		crab_boots = new CrabArmor("crab_boots", 1, EntityEquipmentSlot.FEET);
 	}
 	
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		event.getRegistry().registerAll(
 			emerald_idol, diamond_skull, gold_tablet,
-			iron_ring, prismarin_cross
+			iron_ring, prismarin_cross, crab_shell_piece,
+			crab_helmet, crab_chestplate, crab_leggings,
+			crab_boots
 		);
 	}
 	
@@ -43,6 +62,13 @@ public class MOTDItems {
 		registerRender(gold_tablet);
 		registerRender(iron_ring);
 		registerRender(prismarin_cross);
+		
+		registerRender(crab_shell_piece);
+		
+		registerRender(crab_helmet);
+		registerRender(crab_chestplate);
+		registerRender(crab_leggings);
+		registerRender(crab_boots);
 	}
 	
 	private static void registerRender(Item item) {
