537b3f774da6c683bd5219fea13c2b39a8af855d
Daniel Hajjar <daniel.hajjar@hotmail.se>
2018-05-31 23:39:22 +0200

    Added ocean totem

--- src/main/java/motd/handlers/MOTDBlocks.java
+++ src/main/java/motd/handlers/MOTDBlocks.java
@@ -2,6 +2,7 @@ package motd.handlers;
 
 import motd.blocks.BaseBlock;
 import motd.blocks.BlockMossyQuartzPillar;
+import motd.blocks.BlockOceanTotem;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
@@ -21,17 +22,20 @@ public class MOTDBlocks {
 	public static Block quartz_bricks;
 	public static Block quartz_bricks_mossy;
 	public static Block quartz_column_mossy;
+	public static Block ocean_totem;
 	
 	public static void init() {
 		quartz_bricks = new BaseBlock("quartz_bricks", Material.ROCK, MapColor.QUARTZ).setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
 		quartz_bricks_mossy = new BaseBlock("quartz_bricks_mossy", Material.ROCK, MapColor.QUARTZ).setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
 		quartz_column_mossy = new BlockMossyQuartzPillar();
+		ocean_totem = new BlockOceanTotem();
 	}
 	
 	@SubscribeEvent
 	public static void registerBlocks(RegistryEvent.Register<Block> event) {
 		event.getRegistry().registerAll(
-			quartz_bricks, quartz_bricks_mossy, quartz_column_mossy
+			quartz_bricks, quartz_bricks_mossy, quartz_column_mossy,
+			ocean_totem
 		);
 	}
 	
@@ -40,7 +44,8 @@ public class MOTDBlocks {
 		event.getRegistry().registerAll(
 			new ItemBlock(quartz_bricks).setRegistryName(quartz_bricks.getRegistryName()),
 			new ItemBlock(quartz_bricks_mossy).setRegistryName(quartz_bricks_mossy.getRegistryName()),
-			new ItemBlock(quartz_column_mossy).setRegistryName(quartz_column_mossy.getRegistryName())
+			new ItemBlock(quartz_column_mossy).setRegistryName(quartz_column_mossy.getRegistryName()),
+			new ItemBlock(ocean_totem).setRegistryName(ocean_totem.getRegistryName())
 		);
 	}
 	
@@ -49,6 +54,7 @@ public class MOTDBlocks {
 		registerRender(Item.getItemFromBlock(quartz_bricks));
 		registerRender(Item.getItemFromBlock(quartz_bricks_mossy));
 		registerRender(Item.getItemFromBlock(quartz_column_mossy));
+		registerRender(Item.getItemFromBlock(ocean_totem));
 	}
 	
 	private static void registerRender(Item item) {
