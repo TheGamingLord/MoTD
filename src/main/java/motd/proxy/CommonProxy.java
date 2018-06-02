fa1330c94b1afd9ae045ddead00eaae0a1323de3
Reuben Renquist <seedofdiscovery@gmail.com>
2018-06-02 11:17:48 -0700

    Removed in favor of a common proxy

---
+++ src/main/java/motd/proxy/CommonProxy.java
@@ -0,0 +1,26 @@
+package motd.proxy;
+
+import motd.handlers.MOTDBlocks;
+import motd.handlers.MOTDItems;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+
+	public void preInit(FMLPreInitializationEvent event) {
+		MOTDItems.init();
+		MOTDBlocks.init();
+	}
+
+	public void init(FMLInitializationEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void postInit(FMLPostInitializationEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
