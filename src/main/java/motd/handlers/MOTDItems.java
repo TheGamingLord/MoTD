a2d4332904de25a1d8e2872d39c01886464be59e
Reuben Renquist <seedofdiscovery@gmail.com>
2018-06-02 12:56:12 -0700

    Removed Subscriber

--- src/main/java/motd/handlers/MOTDItems.java
+++ src/main/java/motd/handlers/MOTDItems.java
@@ -8,10 +8,8 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 
-@Mod.EventBusSubscriber
 public class MOTDItems {
 
 	@ObjectHolder(References.MODID + ":emerald_idol")
